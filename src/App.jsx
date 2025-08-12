d1f190bc235222112d4168dc6e6cfe5a2b2d5e6c
IAMGOODNESS <goodnesspeters77@gmail.com>
2025-08-12 11:24:16 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Sidebar from './student components/sidebar'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <Sidebar/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/student components/sidebar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  FaTachometerAlt,
+  FaEnvelope,
+  FaBell,
+  FaTasks,
+  FaNewspaper,
+  FaChartBar,
+  FaUpload,
+  FaUser,
+  FaPowerOff,
+} from "react-icons/fa";
+
+
+export default function Sidebar() {
+  const menuItems = [
+    { name: "Dashboard", icon: <FaTachometerAlt /> },
+    { name: "Messages", icon: <FaEnvelope /> },
+    { name: "Notification", icon: <FaBell /> },
+    { name: "Ongoing Task", icon: <FaTasks /> },
+    { name: "News feed", icon: <FaNewspaper /> },
+    { name: "Leaderboard", icon: <FaChartBar /> },
+    { name: "Submissions", icon: <FaUpload /> },
+    { name: "View profile", icon: <FaUser /> },
+  ];
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar-logo">SmartLink</div>
+      <ul className="sidebar-menu">
+        {menuItems.map((item, index) => (
+          <li key={index}>
+            <div style={{color:"#1E3AEA"}}>
+        {item.icon}
+            </div>
+           
+            <span>{item.name}</span>
+          </li>
+        ))}
+      </ul>
+      <div className="sidebar-logout">
+        <FaPowerOff />
+        <span>Logout</span>
+      </div>
+    </div>
+  );
+}
